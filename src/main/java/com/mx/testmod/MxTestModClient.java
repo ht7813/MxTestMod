c2ff3427fa6256e4c7b180ec027764c7a59a4e92
ht7813 <hutao7813@126.com>
2023-06-23 20:30:42 +0800

    Initial commit

---
+++ src/main/java/com/mx/testmod/MxTestModClient.java
@@ -0,0 +1,10 @@
+package com.mx.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MxTestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
