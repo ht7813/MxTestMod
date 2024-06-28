26fe28aa165821e3b5190cb229f78c7b64a74ba4
ht7813 <114791222+ht7813@users.noreply.github.com>
2024-06-28 10:42:15 +0800

    Add Armors

---
+++ src/main/java/com/mx/testmod/material/CitrineArmorMaterial.java
@@ -0,0 +1,88 @@
+package com.mx.testmod.material;
+
+import com.mx.testmod.registry.ModItems;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Util;
+
+import java.util.EnumMap;
+import java.util.function.Supplier;
+
+public enum CitrineArmorMaterial implements ArmorMaterial {
+    CITRINE("citrine", 5, Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+        map.put(ArmorItem.Type.BOOTS, 3);
+        map.put(ArmorItem.Type.LEGGINGS, 4);
+        map.put(ArmorItem.Type.CHESTPLATE, 5);
+        map.put(ArmorItem.Type.HELMET, 3);
+    }), 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0f, 0.0f, () -> Ingredient.ofItems(ModItems.CITRINE));
+
+    private static final EnumMap<ArmorItem.Type, Integer> BASE_DURABILITY = Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+        map.put(ArmorItem.Type.BOOTS, 13);
+        map.put(ArmorItem.Type.LEGGINGS, 15);
+        map.put(ArmorItem.Type.CHESTPLATE, 16);
+        map.put(ArmorItem.Type.HELMET, 11);
+    });
+
+    private final String name;
+    private final int durabilityMultiplier;
+    private final EnumMap<ArmorItem.Type, Integer> protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredientSupplier;
+
+    CitrineArmorMaterial(String name, int durabilityMultiplier, EnumMap<ArmorItem.Type, Integer> protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredientSupplier) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredientSupplier = repairIngredientSupplier;
+    }
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return BASE_DURABILITY.get(type) * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return this.protectionAmounts.get(type);
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredientSupplier.get();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
--- src/main/java/com/mx/testmod/registry/ModItems.java
+++ src/main/java/com/mx/testmod/registry/ModItems.java
@@ -1,8 +1,10 @@
 package com.mx.testmod.registry;
 
 import com.mx.testmod.MxTestMod;
+import com.mx.testmod.material.CitrineArmorMaterial;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
@@ -17,6 +19,18 @@ public class ModItems {
     public static final Item RAW_CITRINE = registerItem("raw_citrine",
             new Item(new FabricItemSettings()),
             ModItemGroup.CITRINE, ItemGroups.INGREDIENTS);
+    public static final Item CITRINE_HELMET = registerItem("citrine_helmet",
+            new ArmorItem(CitrineArmorMaterial.CITRINE, ArmorItem.Type.HELMET, new FabricItemSettings()),
+            ModItemGroup.CITRINE, ItemGroups.COMBAT);
+    public static final Item CITRINE_BOOTS = registerItem("citrine_boots",
+            new ArmorItem(CitrineArmorMaterial.CITRINE, ArmorItem.Type.BOOTS, new FabricItemSettings()),
+            ModItemGroup.CITRINE, ItemGroups.COMBAT);
+    public static final Item CITRINE_CHESTPLATE = registerItem("citrine_chestplate",
+            new ArmorItem(CitrineArmorMaterial.CITRINE, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()),
+            ModItemGroup.CITRINE, ItemGroups.COMBAT);
+    public static final Item CITRINE_LEGGINGS = registerItem("citrine_leggings",
+            new ArmorItem(CitrineArmorMaterial.CITRINE, ArmorItem.Type.LEGGINGS, new FabricItemSettings()),
+            ModItemGroup.CITRINE, ItemGroups.COMBAT);
     public static Item registerItem(String name, Item item, ItemGroup... itemGroups) {
         Item registerItem = Registry.register(Registries.ITEM, new Identifier(MxTestMod.MOD_ID, name), item);
         for (ItemGroup itemGroup: itemGroups) {
